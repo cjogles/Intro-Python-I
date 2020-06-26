d140a9032fe3b0f18154b5f79ee9ef57ea70a9a4
cjogles <jackson.ogles28@gmail.com>
2020-06-26 16:27:32 -0600

    finished 13_file_io.py

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -8,12 +8,21 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
-
 # YOUR CODE HERE
+import os
+path = (os.getcwd() + "\src\\foo.txt")
+f = open(path, "r");
+print(f.read())
+f.close();
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
-
 # YOUR CODE HERE
+path = (os.getcwd() + "\src")
+f = open("src\\bar.txt", "w")
+f.write("hello world \n")
+f.write("I'm learning python \n")
+f.write("care to join? \n")
+f.close()
