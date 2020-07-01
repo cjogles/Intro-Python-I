1bb84ea3d4cb7ced50587aba9d5ca729806f15e7
cjogles <jackson.ogles28@gmail.com>
2020-06-30 19:15:56 -0600

    finished prime question

--- src/16_stretch.py
+++ src/16_stretch.py
@@ -52,17 +52,16 @@ print(test1.removeDuplicates([1, 2, 3, 3, 3, 4, 4, 5]))  # should return 5
 # 3. Write a program to determine if a number, given on the command line, is prime.
 
 def solution(num):
-    print(num)
-    if (num == 0):
+    if (num == 0 or num < 0):
         return f"{num} is not a prime number"
     if (num >= 1):
-        for x in range(1, num//2):
+        for x in range(2, num):
             if (num % x) == 0:
                 return f"{num} is not a prime number"
     return f"{num} is a prime number"
     
 # TEST
-print(solution(3)) # should return string ==> "{number}is a prime number."
+print(solution(14)) # should return string ==> "{number}is a prime number."
 
 # My explanation for this problem:
 # - In order for a number to be considered "PRIME", it needs to be greater then 1, and its only factors can be 1 and itself.
