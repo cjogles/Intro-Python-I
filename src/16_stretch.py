66b2e3150176712c4820cf8d52ebe76ec7f7c7a9
cjogles <jackson.ogles28@gmail.com>
2020-06-30 20:09:12 -0600

    working on sieve of eratosthenes

--- src/16_stretch.py
+++ src/16_stretch.py
@@ -52,24 +52,57 @@ print(test1.removeDuplicates([1, 2, 3, 3, 3, 4, 4, 5]))  # should return 5
 # 3. Write a program to determine if a number, given on the command line, is prime.
 
 def solution(num):
-    if (num == 0 or num < 0):
+    if (num < 1):
         return f"{num} is not a prime number"
-    if (num >= 1):
+    else:
         for x in range(2, num):
             if (num % x) == 0:
                 return f"{num} is not a prime number"
     return f"{num} is a prime number"
     
 # TEST
-print(solution(14)) # should return string ==> "{number}is a prime number."
+print(solution(13)) # should return string ==> "{number}is a prime number."
 
 # My explanation for this problem:
 # - In order for a number to be considered "PRIME", it needs to be greater then 1, and its only factors can be 1 and itself.
-# - To solve this, I would first take care of the edge case of it being less then 1. 
+# - To solve this, I would first take care of the edge case of it being less then 1, if so, its not prime.
+# - next, I realize that every non-negative integer greater then 0 has 1 and itself as its factors. I just need to check if any other
+#   number is a factor of the given number. So I take 2 through (num -1), loop through each one of them and run the following:
+#   num % x == 0... if that evaluates to true, that means there is a factor of num outside of 1 and itself, which makes it NOT prime. 
+#   otherwise return prime
 
 # STRETCH TASK 4
 # How can you optimize this program?
+# ---- going to skip this question for now.
 
 # STRETCH TASK 5
+# https://www.khanacademy.org/computing/computer-science/cryptography/comp-number-theory/v/sieve-of-eratosthenes-prime-adventure-part-4
 # Implement [The Sieve of Eratosthenes](https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes), one
 # of the oldest algorithms known (ca. 200 BC).
+# this algorithim finds all prime numbers up to a given limit
+
+import math
+
+def sieve_of_eratosthenes(num):
+    print("heyhey", math.floor(math.sqrt(num)))
+    for x in range(2, (math.floor(math.sqrt(num)))):
+        print(x, num)
+        if (x == num):
+            return "done"
+
+print (sieve_of_eratosthenes(21))
+# My explanation for this problem:
+# - Basically I just follow the sieve of eratosthenes algorithim to solve this problem. It works in the following way:
+# - All numbers from 2 - the limit given are possible prime numbers. At the beginning, they are all considered "unmarked"
+# - Start iterating over each number. If you come across an "unmarked" number, that means its prime.
+# - So, you would reach the first number, 2, and you would "mark it" therefore declaring your first prime number. 
+# - After you mark a prime number, you can now "mark" all multiples of that marked number (2), because we know they are composite (i.e. not prime)
+# - After you mark every multiple of 2, you can repeat. The next "unmarked" number is 3. Add this to your list of primes that you collect.
+# - Same as before, you can now take multiples of the newly "marked" number (3), and "mark" each of them, declaring them NOT prime. 
+# - You'll notice that "6" is a multiple of BOTH "2" AND "3". So a neat optimizaation to this algorithim is always start at the square of the newly
+#   marked number, when you are "marking off" new composite numbers to declare them NOT prime. That way you don't waste your time looking to "mark" 
+#   off "6" again.
+# - Continue! You'll come across a "marked" number, "4". Since its "marked", you can skip it and you'll come to "5". Repeat the same process
+#   you did with "3 and your on your way!"
+# - Eventually you'll get to an "unmarked" number whose square is greater then the original limit given at the beginning. Once you've reached
+#   this point that means the rest of the unmarked numbers ARE PRIME. Fill in the rest of them as prime and your done!
